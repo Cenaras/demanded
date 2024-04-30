ae4668cab11961c2ba33f8aff232afcab126cf19
Mathias Rud <mrud@cs.au.dk>
2024-04-30 10:47:58 +0200

    Comparison of solution sizes

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -7,10 +7,9 @@ def main(): Unit = {
 //    compareMagicToHeintzeTardieu(100, 7, 3, 1)
   datalogAnalysisCost()
 
-Alt2().compileAndAnalyze(Parser.ParseTemplate("slides"), 5)
-
 }
 
+// TODO: Implement these in the framework and compare then in the CompareDemanded test case
 private def datalogAnalysisCost(): Unit = {
   val times = 1000
   val size = 7
@@ -20,10 +19,12 @@ private def datalogAnalysisCost(): Unit = {
   var standardCheaper = 0
   var alt1Cheaper = 0
   var htCheaper = 0
+  var alt2Cheaper = 0
 
   var standardSolSizeSmallest = 0
-  var al1SolSizeSmallest = 0
+  var alt1SolSizeSmallest = 0
   var htSolSizeSmallest = 0
+  var alt2SolSizeSmallest = 0
 
   for i <- 0 until times do
     val seed = scala.util.Random.nextInt()
@@ -47,11 +48,20 @@ private def datalogAnalysisCost(): Unit = {
     val htSol = ht.solve(p, q)
     val htCost = ht.cost
 
-    val (unique1, smallest1) = uniqueSmallest(List(standardCost, alt1Cost, htCost))
+
+    val alt2 = Alt2()
+    val alt2OutPath = "untitled/alt2Out.tsv"
+    alt2.compileAndAnalyze(p, q)
+    alt2.outputSolution(alt2OutPath)
+    val alt2Cost = alt2.cost(alt2OutPath)
+
+
+    val (unique1, smallest1) = uniqueSmallest(List(standardCost, alt1Cost, htCost, alt2Cost))
     if unique1 then
       if smallest1 == standardCost then standardCheaper += 1
       if smallest1 == alt1Cost then alt1Cheaper += 1
       if smallest1 == htCost then htCheaper += 1
+      if smallest1 == alt2Cost then alt2Cheaper += 1
 
 
 
@@ -59,20 +69,24 @@ private def datalogAnalysisCost(): Unit = {
     val htSolSize = htSol.foldLeft(0)((a, sol) => {a + sol.size})
     val standardSolSize = standard.readSolution(standardOutPath).linesIterator.size
     val alt1SolSize = alt1.readSolution(alt1OutPath).linesIterator.size
+    val alt2SolSize = alt2.readSolution(alt2OutPath).linesIterator.size
 
-     val (unique2, smallest2) = uniqueSmallest(List(htSolSize, standardSolSize, alt1SolSize))
+     val (unique2, smallest2) = uniqueSmallest(List(htSolSize, standardSolSize, alt1SolSize, alt2SolSize))
     if unique2 then
       if smallest2 == htSolSize then htSolSizeSmallest += 1
       if smallest2 == standardSolSize then standardSolSizeSmallest += 1
-      if smallest2 == alt1SolSize then al1SolSizeSmallest += 1
+      if smallest2 == alt1SolSize then alt1SolSizeSmallest += 1
+      if smallest2 == alt2SolSize then alt2SolSizeSmallest += 1
 
   println(s"Standard was cheapest $standardCheaper times")
   println(s"Alt1 was cheapest $alt1Cheaper times")
   println(s"Heintze-Tardieu was cheapest $htCheaper times")
+  println(s"Alt2 was cheapest $alt2Cheaper times")
 
   println(s"Standard solution was smallest $standardSolSizeSmallest times")
-  println(s"Alt1 solution was smallest $al1SolSizeSmallest times")
+  println(s"Alt1 solution was smallest $alt1SolSizeSmallest times")
   println(s"Heintze-Tardieu solution was smallest $htSolSizeSmallest times")
+  println(s"Alt2 solution was smallest $alt2SolSizeSmallest times")
 }
 
 
