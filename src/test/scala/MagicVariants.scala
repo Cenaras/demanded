391ef3dd8e8485c7c31d2e7b9171d6e60eacf089
Mathias Rud <mrud@cs.au.dk>
2024-07-05 16:21:58 +0200

    Small experiments on cost of different magic sets analyses

--- src/test/scala/MagicVariants.scala
+++ src/test/scala/MagicVariants.scala
@@ -5,21 +5,48 @@ class MagicVariants extends AnyFunSuite {
   // Results for 1.000.000 runs with distribution (5, 15, 2)
   // Value v at index i means that analysis i cost the most for v programs
 
+  /** Most expensive computation - removing the worst performing in each iteration */
+
   // m0, m1, m2, m6, m7
+  //  most expensive:
   //  (1139, 5181, 591433, 18743, 24599)
+  //  cheapest:
+  //  (21328, 13378, 330, 35170, 29368)
+
 
   // m0, m1, m6, m7
+  //  most expensive:
   //  (72435, 44938, 21400, 28308)
+  //  cheapest:
+  //  (21944, 13518, 35149, 29515)
 
+  // From here the two runs do not follow so we split them up
+  // Expensive results:
   // m1, m6, m7
+  //  most expensive:
   //  (467330, 29217, 29909)
 
-  // m1, m7
-  // (478312, 188162)
+  // m6, m7
+  //  most expensive:
+  //  (51849, 63341)
+
+  // Cheapest results:
+  // m0, m6, m7
+  // cheapest:
+  //  (181318, 36107, 32689)
+
+  // m0, m6
+  // cheapest:
+  //  (185694, 487007)
+
 
 
   test("Compare costs") {
+    // Amount of times index i was the most expensive
     val highest: Array[Int] = new Array[Int](5)
+    // Amount of times index i was the cheapest
+    val lowest: Array[Int] = new Array[Int](5)
+
     for i <- 0 until 1000000 do {
 
       if i != 0 && i % 10000 == 0 then
@@ -41,8 +68,6 @@ class MagicVariants extends AnyFunSuite {
       val m7 = new MagicSet7()
       m7.solve(p, q)
 
-
-
       val costs = Array(m0.cost, m1.cost, m2.cost, m6.cost, m7.cost)
       var largest = -1
       var maxIndex = -1
@@ -59,11 +84,27 @@ class MagicVariants extends AnyFunSuite {
         highest(maxIndex) += 1
       }
 
+      var smallest = Int.MaxValue
+      var minIndex = -1
+      for (i <- costs.indices) {
+        if (costs(i) < smallest) {
+          smallest = costs(i)
+          minIndex = i
+        }
+      }
+
+      val occurrences = costs.count(p => p == smallest)
+      if (occurrences == 1) {
+        lowest(minIndex) += 1
+      }
 
     }
 
-    println("Distribution of costs where solution must have been uniquely the largest")
+    println("Distribution of most expensive where solution must have been uniquely the largest")
     println(highest.mkString("Array(", ", ", ")"))
+    println()
+    println("Distribution of cheapest where solution must have been uniquely the smallest")
+    println(lowest.mkString("Array(", ", ", ")"))
 
   }
 
