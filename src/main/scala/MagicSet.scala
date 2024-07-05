b0f137f83b5e949c5e47fa2ce8825462a099641f
Mathias Rud <mrud@cs.au.dk>
2024-07-05 15:47:46 +0200

    Small experiments on different magic set implementations

--- src/main/scala/MagicSet.scala
+++ src/main/scala/MagicSet.scala
@@ -2,85 +2,75 @@ import scala.collection.mutable
 
 trait MagicSet {
 
-  var changed = false 
-  
-  var pt_bf= mutable.Map[Var, mutable.Set[Token]]().withDefaultValue(mutable.Set.empty)
+  var changed = false
+
+  var pt_bf = mutable.Map[Var, mutable.Set[Token]]().withDefaultValue(mutable.Set.empty)
   var pt_bb = mutable.Map[Var, mutable.Set[Token]]().withDefaultValue(mutable.Set.empty)
   var pt_fb = mutable.Map[Var, mutable.Set[Token]]().withDefaultValue(mutable.Set.empty)
 
-  var pt_bbf= mutable.Map[(Token, Field), mutable.Set[Token]]().withDefaultValue(mutable.Set.empty)
+  var pt_bbf = mutable.Map[(Token, Field), mutable.Set[Token]]().withDefaultValue(mutable.Set.empty)
   var pt_bbb = mutable.Map[(Token, Field), mutable.Set[Token]]().withDefaultValue(mutable.Set.empty)
 
   var pt_fbb = mutable.Map[(Token, Field), mutable.Set[Token]]().withDefaultValue(mutable.Set.empty)
   var pt_fbf = mutable.Map[(Token, Field), mutable.Set[Token]]().withDefaultValue(mutable.Set.empty)
-  
+
   var magic_bf = mutable.Set[Token]()
   var magic_bbf = mutable.Set[(Token, Field)]()
-  
+
   var magic_bb = mutable.Map[Var, mutable.Set[Token]]().withDefaultValue(mutable.Set.empty)
   var magic_bbb = mutable.Map[(Token, Field), mutable.Set[Token]]().withDefaultValue(mutable.Set.empty)
-  
+
   var magic_fbb = mutable.Map[Field, mutable.Set[Token]]().withDefaultValue(mutable.Set.empty)
   var magic_fbf = mutable.Set[Field]()
-  
+
   var magic_fb = mutable.Set[Token]()
-  
-  
-  
+
+
   private def addMapSet[K, V](m: mutable.Map[K, mutable.Set[V]], k: K, v: V) = {
     if !m.contains(k) then
       val fresh = mutable.Set[V]()
       m += k -> fresh
-    
+
     changed |= m(k).add(v)
   }
 
 
   def addMagicBF(x: Var) = changed |= magic_bf.add(x)
+
   def addMagicBBF(t: Token, f: Field) = changed |= magic_bbf.add(t, f)
-  
+
   def addMagicBB(x: Var, t: Token) = addMapSet(magic_bb, x, t)
+
   def addMagicBBB(t1: Token, f: Field, t2: Token) = addMapSet(magic_bbb, (t1, f), t2)
-  
+
   def addMagicFBB(f: Field, t: Token) = addMapSet(magic_fbb, f, t)
+
   def addMagicFBF(f: Field) = changed |= magic_fbf.add(f)
-  
+
   def addPtBF(x: Var, t: Token) = addMapSet(pt_bf, x, t)
+
   def addPtBB(x: Var, t: Token) = addMapSet(pt_bb, x, t)
-  
+
   def addPtBBF(t1: Token, f: Field, t2: Token) = addMapSet(pt_bbf, (t1, f), t2)
+
   def addPtBBB(t1: Token, f: Field, t2: Token) = addMapSet(pt_bbb, (t1, f), t2)
-  
+
   def addPtFBB(t1: Token, f: Field, t2: Token) = addMapSet(pt_fbb, (t1, f), t2)
+
   def addPtFBF(t1: Token, f: Field, t2: Token) = addMapSet(pt_fbf, (t1, f), t2)
-  
+
   def addPtFB(x: Var, t: Token) = addMapSet(pt_fb, x, t)
-  
+
   def addMagicFB(t: Token) = changed |= magic_fb.add(t)
-  
+
   def solve(p: Program, q: Var) = {
     addMagicBF(q) // (9)
-    
+
     while (changed) {
       changed = false
       for i <- p.getInstructions do
         process(i)
     }
-
-//    println("pt_bf")
-//    println(pt_bf)
-//    println("pt_bb")
-//    println(pt_bb)
-//    println("pt_fb")
-//    println(pt_fb)
-//    println("pt_bbf")
-//    println(pt_bbf)
-//    println("pt_bbb")
-//    println(pt_bbb)
-//    println("pt_fbb")
-//    println(pt_fbb)
-//    println("pt_fbf")
-//    println(pt_fbf)
 
     mergeSolutions()
 
@@ -109,6 +99,31 @@ trait MagicSet {
     res
   }
 
+  def cost: Int = {
+
+
+    var cost = 0
+    cost += pt_bf.values.foldLeft(0)((a, b) => a+b.size)
+    cost += pt_bb.values.foldLeft(0)((a, b) => a + b.size)
+    cost += pt_fb.values.foldLeft(0)((a, b) => a + b.size)
+    cost += pt_bbf.values.foldLeft(0)((a, b) => a + b.size)
+    cost += pt_bbb.values.foldLeft(0)((a, b) => a + b.size)
+    cost += pt_fbb.values.foldLeft(0)((a, b) => a + b.size)
+    cost += pt_fbf.values.foldLeft(0)((a, b) => a + b.size)
+
+    cost += magic_bf.size
+    cost += magic_bbf.size
+
+    cost += magic_bb.values.foldLeft(0)((a, b) => a + b.size)
+    cost += magic_bbb.values.foldLeft(0)((a, b) => a + b.size)
+
+    cost += magic_fbb.values.foldLeft(0)((a, b) => a + b.size)
+    cost += magic_fbf.size
+
+    cost += magic_fb.size
+
+    cost
+  }
 
 
 
--- src/test/scala/MagicVariants.scala
+++ src/test/scala/MagicVariants.scala
@@ -2,30 +2,68 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class MagicVariants extends AnyFunSuite {
 
+  // Results for 1.000.000 runs with distribution (5, 15, 2)
+  // Value v at index i means that analysis i cost the most for v programs
 
-  test("qwe") {
+  // m0, m1, m2, m6, m7
+  //  (1139, 5181, 591433, 18743, 24599)
 
-    for _ <- 0 until 100000 do {
+  // m0, m1, m6, m7
+  //  (72435, 44938, 21400, 28308)
+
+  // m1, m6, m7
+  //  (467330, 29217, 29909)
+
+  // m1, m7
+  // (478312, 188162)
+
+
+  test("Compare costs") {
+    val highest: Array[Int] = new Array[Int](5)
+    for i <- 0 until 1000000 do {
+
+      if i != 0 && i % 10000 == 0 then
+        println(s"Finished ${i} iterations")
+
       val seed = scala.util.Random.nextInt() // 5, 15, 2
       val g = new SimpleProgramGenerator(seed, 5, 15, 2)
       val p = g.generate()
       val q = g.genQuery
 
-      val ex = new NaiveExhaustiveSolver()
-      val m = new MagicSet7()
+      val m0 = new MagicSet0()
+      val sol = m0.solve(p, q)
+      val m1 = new MagicSet1()
+      m1.solve(p, q)
+      val m2 = new MagicSet2()
+      m2.solve(p, q)
+      val m6 = new MagicSet6()
+      m6.solve(p, q)
+      val m7 = new MagicSet7()
+      m7.solve(p, q)
 
-      val exSol = ex.solve(p)
-      val mSol = m.solve(p, q)
 
-      if !(exSol.get(q) == mSol.get(q)) then
-        println(s"Mismatch in solutions for program with query ${q} and seed ${seed}:")
-        p.print()
-        println("ExSol:")
-        println(exSol)
-        println("mSol:")
-        println(mSol)
-        throw Error()
+
+      val costs = Array(m0.cost, m1.cost, m2.cost, m6.cost, m7.cost)
+      var largest = -1
+      var maxIndex = -1
+      for (i <- costs.indices) {
+        if (costs(i) > largest) {
+          largest = costs(i)
+          maxIndex = i
+        }
+      }
+
+      // Check if largest value was unique
+      val maxOccurrences = costs.count(p => p == largest)
+      if (maxOccurrences == 1) {
+        highest(maxIndex) += 1
+      }
+
+
     }
+
+    println("Distribution of costs where solution must have been uniquely the largest")
+    println(highest.mkString("Array(", ", ", ")"))
 
   }
 
